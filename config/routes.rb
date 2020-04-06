cdd4111333da488ec380fb75525e26f5b8c92a8c
はまひつじ <hamahitsuji@hamahitsuji-Pro.local>
2020-04-06 10:19:46 +0900

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'todolists/new'
+  get 'todolists/new' #コントローラとアクションを書いていない場合はURLから勝手に判断される
   get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
 
 end
