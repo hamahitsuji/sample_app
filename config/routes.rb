63db83867be437e81e825be3e2e2b9d7d6a9b0a5
vagrant <vagrant@localhost.localdomain>
2020-04-06 08:09:20 +0000

    [Finish] 3-9

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'updatea_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 
 end
